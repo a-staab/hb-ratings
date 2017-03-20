cb40d0a4a8dc998f14ccc166acfb38aebc4d2c2b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-20 16:13:34 -0700

    added movie and rating classes

--- model.py
+++ model.py
@@ -26,6 +26,26 @@ class User(db.Model):
 
 # Put your Movie and Rating model classes here.
 
+class Movie(db.Model):
+    """Movie users rate"""
+
+    __tablename__ = "movies"
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    released_at = db.Column(db.DateTime, nullable=False)
+    imdb_url = db.Column(db.String(50), nullable=True)
+
+
+class Rating(db.Model):
+    "User's rating of movie"
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
 
 ##############################################################################
 # Helper functions
